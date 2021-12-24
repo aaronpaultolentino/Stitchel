12dbc426d0e1ebb9c8818f831675525442152312
aaronpaultolentino <aaron@eventleap.com>
2021-12-24 23:22:54 +0800

    Unit Test Search & Update profile

--- tests/Feature/WebStitchelTest/UpdateProfileControllerTest.php
+++ tests/Feature/WebStitchelTest/UpdateProfileControllerTest.php
@@ -10,20 +10,20 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UpdateProfileControllerTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
+    use WithFaker;
 
 
     /**
      * A basic feature LoginForm test.
      *
      * @test *
-     * @group login *
+     * @group userUpdate *
      */
-    public function user_can_view_edit_profile()
+    public function user_can_view_update_profile()
     {
         $user = factory(User::class)->create();
 
-        $this->actingAs($user)->get('/profile')
+        $this->actingAs($user, 'web')->get('/profile')
             ->assertStatus(200);
     }
 
@@ -31,15 +31,90 @@ class UpdateProfileControllerTest extends TestCase
      * A basic feature RequiredMoileNumber test.
      *
      * @test *
-     * @group login *
+     * @group userUpdate *
      */
       public function mobile_number_is_required()
     {
 
         $user = factory(User::class)->make(['mobile_number' => null,]);
 
-        $this->actingAs($user)->post('/profile/profile', $user->toArray())
+        $this->actingAs($user, 'web')->post('/profile/profile', $user->toArray())
             ->assertSessionHasErrors(('mobile_number'));
+    }
+
+     /**
+     * A basic feature RequiredAddress test.
+     *
+     * @test *
+     * @group userUpdate *
+     */
+      public function address_is_required()
+    {
+
+        $user = factory(User::class)->make(['address' => null,]);
+
+        $this->actingAs($user, 'web')->post('/profile/profile', $user->toArray())
+            ->assertSessionHasErrors(('address'));
+    }
+
+     /**
+     * A basic feature RequiredPostcode test.
+     *
+     * @test *
+     * @group userUpdate *
+     */
+      public function postcode_is_required()
+    {
+
+        $user = factory(User::class)->make(['postcode' => null,]);
+
+        $this->actingAs($user, 'web')->post('/profile/profile', $user->toArray())
+            ->assertSessionHasErrors(('postcode'));
+    }
+
+     /**
+     * A basic feature RequiredArea test.
+     *
+     * @test *
+     * @group userUpdate *
+     */
+      public function area_is_required()
+    {
+
+        $user = factory(User::class)->make(['area' => null,]);
+
+        $this->actingAs($user, 'web')->post('/profile/profile', $user->toArray())
+            ->assertSessionHasErrors(('area'));
+    }
+
+    /**
+     * A basic feature RequiredCountry test.
+     *
+     * @test *
+     * @group userUpdate *
+     */
+      public function country_is_required()
+    {
+
+        $user = factory(User::class)->make(['country' => null,]);
+
+        $this->actingAs($user, 'web')->post('/profile/profile', $user->toArray())
+            ->assertSessionHasErrors(('country'));
+    }
+
+     /**
+     * A basic feature RequiredState test.
+     *
+     * @test *
+     * @group userUpdate *
+     */
+      public function state_is_required()
+    {
+
+        $user = factory(User::class)->make(['state' => null,]);
+
+        $this->actingAs($user, 'web')->post('/profile/profile', $user->toArray())
+            ->assertSessionHasErrors(('state'));
     }
 
     /**
@@ -51,10 +126,9 @@ class UpdateProfileControllerTest extends TestCase
     public function user_can_update_profile()
     {
     
-        $user = factory(User::class)->create();
+        $user = factory(User::class)->make();
 
-
-        $this->actingAs($user);
-
+        $this->assertTrue($user->save());
+        $this->actingAs($user, 'web')->post('/profile/profile', $user->toArray());
     }
 }
